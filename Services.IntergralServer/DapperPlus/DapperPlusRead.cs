3901617305fd92ad0edff161a3a00f898d9343ed
fanjh <32724179+fanjunhan@users.noreply.github.com>
2021-08-16 17:38:55 +0800

    Add files via upload

---
+++ Services.IntergralServer/DapperPlus/DapperPlusRead.cs
@@ -0,0 +1,93 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Services.IntergralServer.DapperPlus
+{
+
+    /// <summary>
+    /// DapperPlus读接口
+    /// </summary>
+    public interface IDapperPlusRead
+    {
+        /// <summary>
+        /// 连接
+        /// </summary>
+        public IDbConnection Connection
+        {
+            get;
+        }
+        /// <summary>
+        /// Executes a single-row query, returning the data typed as type.
+        /// </summary>
+        /// <param name="type">The type to return.</param>
+        /// <param name="sql">The SQL to execute for the query.</param>
+        /// <param name="param">The parameters to pass, if any.</param>
+        /// <param name="transaction">The transaction to use, if any.</param>
+        /// <param name="buffered">Whether to buffer results in memory.</param>
+        /// <param name="commandTimeout">The command timeout (in seconds).</param>
+        /// <param name="commandType">The type of command to execute.</param>
+        /// <returns>A sequence of data of the supplied type; if a basic type(int, string, etc) is queried then the data from the first column in assumed, otherwise an instance is created per row, and a direct column-name===member-name mapping is assumed(case insensitive).
+        /// 异常:T:System.ArgumentNullException:type is null.
+        /// </returns>
+        Task<IList<T>> QueryAsync<T>(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null);
+
+        Task<T> QueryFirstAsync<T>(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null);
+    }
+    /// <summary>
+    /// DapperPlus读实现类
+    /// </summary>
+    public class DapperPlusRead : IDapperPlusRead
+    {
+        protected IDbConnection _connection;
+        /// <summary>
+        /// 构造
+        /// </summary>
+        /// <param name="connection">连接</param>
+        /// <param name="configuration">配置</param>
+        public DapperPlusRead(IDbConnection connection, IConfiguration configuration)
+        {
+            var connectionStrings = configuration.GetSection("ConnectionStrings").Get<Dictionary<string, string>>();
+            _connection = connection;
+            _connection.ConnectionString = connectionStrings.Where(s => s.Key.ToLower().Contains("read")).FirstOrDefault().Value;
+        }
+        /// <summary>
+        /// 连接
+        /// </summary>
+        public IDbConnection Connection
+        {
+            get
+            {
+                return _connection;
+            }
+        }
+
+        /// <summary>
+        /// Executes a single-row query, returning the data typed as type.
+        /// </summary>
+        /// <param name="type">The type to return.</param>
+        /// <param name="sql">The SQL to execute for the query.</param>
+        /// <param name="param">The parameters to pass, if any.</param>
+        /// <param name="transaction">The transaction to use, if any.</param>
+        /// <param name="buffered">Whether to buffer results in memory.</param>
+        /// <param name="commandTimeout">The command timeout (in seconds).</param>
+        /// <param name="commandType">The type of command to execute.</param>
+        /// <returns>A sequence of data of the supplied type; if a basic type(int, string, etc) is queried then the data from the first column in assumed, otherwise an instance is created per row, and a direct column-name===member-name mapping is assumed(case insensitive).
+        /// 异常:T:System.ArgumentNullException:type is null.
+        /// </returns>
+        public async Task<IList<T>> QueryAsync<T>(string sql, object param = null, IDbTransaction transaction = null,  int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return (await _connection.QueryAsync<T>(sql, param, transaction, commandTimeout, commandType)).ToList();
+        }
+
+
+        public async Task<T> QueryFirstAsync<T>(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return await _connection.QueryFirstAsync<T>(sql, param, transaction, commandTimeout, commandType);
+        }
+    }
+}
---
+++ Services.IntergralServer/DapperPlus/DapperPlusWrite.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace Services.IntergralServer.DapperPlus
+{
+    /// <summary>
+    /// 写Dapper接口
+    /// </summary>
+    public interface IDapperPlusWrite
+    {
+        /// <summary>
+        /// 连接
+        /// </summary>
+        public IDbConnection Connection
+        {
+            get;
+        }
+        /// <summary>
+        /// Execute parameterized SQL.
+        /// </summary>
+        /// <param name="sql">The SQL to execute for this query.</param>
+        /// <param name="param">The parameters to use for this query.</param>
+        /// <param name="transaction">The transaction to use for this query.</param>
+        /// <param name="commandTimeout">Number of seconds before command execution timeout.</param>
+        /// <param name="commandType">Is it a stored proc or a batch?</param>
+        /// <returns>The number of rows affected.</returns>
+        Task<int> ExecuteAsync(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null);
+
+    }
+
+    /// <summary>
+    /// 写Dapper类
+    /// </summary>
+    public class DapperPlusWrite : IDapperPlusWrite
+    {
+        private readonly IDbConnection _connection;
+        /// <summary>
+        /// 构造
+        /// </summary>
+        /// <param name="connection">连接</param>
+        /// <param name="configuration">配置</param>
+        public DapperPlusWrite(IDbConnection connection, IConfiguration configuration)
+        {
+            var connectionStrings = configuration.GetSection("ConnectionStrings").Get<Dictionary<string, string>>();
+            _connection = connection;
+            _connection.ConnectionString = connectionStrings.Where(s => s.Key.ToLower().Contains("write")).FirstOrDefault().Value;
+        }
+        /// <summary>
+        /// 连接
+        /// </summary>
+        public IDbConnection Connection
+        {
+            get
+            {
+                return _connection;
+            }
+        }
+        /// <summary>
+        /// Execute parameterized SQL.
+        /// </summary>
+        /// <param name="sql">The SQL to execute for this query.</param>
+        /// <param name="param">The parameters to use for this query.</param>
+        /// <param name="transaction">The transaction to use for this query.</param>
+        /// <param name="commandTimeout">Number of seconds before command execution timeout.</param>
+        /// <param name="commandType">Is it a stored proc or a batch?</param>
+        /// <returns>The number of rows affected.</returns>
+        public async Task<int> ExecuteAsync(string sql, object param = null, IDbTransaction transaction = null, int? commandTimeout = null, CommandType? commandType = null)
+        {
+            return await _connection.ExecuteAsync(sql, param, transaction, commandTimeout, commandType);
+        }
+   
+    }
+}
