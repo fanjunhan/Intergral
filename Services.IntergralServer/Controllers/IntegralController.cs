e0c96fb12b52fbda097ac4c27edffd4d64208e98
fanjh <32724179+fanjunhan@users.noreply.github.com>
2021-08-16 17:03:24 +0800

    Add files via upload

--- Services.IntergralServer/Controllers/IntegralController.cs
+++ Services.IntergralServer/Controllers/IntegralController.cs
@@ -1,15 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
-using YLH.Infrasturecture.Dapper;
 using Integral.Models;
 using Microsoft.Extensions.Logging;
 using Dapr.Client;
 using Integral.Actors;
 using Integral.WebApi.Encryption;
 using Services.WebApi;
-using YLH.Infrastructure.ExternalSign;
 using System.Collections.Generic;
+using Services.IntergralServer.DapperPlus;
+using Services.IntergralServer;
 
 namespace Integral.WebApi.Controllers
 {
@@ -18,9 +18,11 @@ namespace Integral.WebApi.Controllers
     [Route("[controller]")]
     public class IntegralController : Controller
     {
-        private static DapperClient _dapperClient;
+
         private readonly ILogger<IntegralController> _logger;
         private readonly IIntegralConfigStore configStore;
+        private readonly IDapperPlusWrite _dapperPlusWrite;
+        private readonly IDapperPlusRead _dapperPlusRead;
         private const string spendSql = "INSERT INTO integral_cost (UserId,DataId,Title,AddTime,Score,AreaCode) VALUES (@UserId,@ItemId,@Title,@AddTime,@Score,@AreaCode);";
         private const string updateSql = "UPDATE integral_area set PayCount = PayCount+@Score where Integral_Userid=@UserId and AreaCode=@AreaCode and Integral_Count>=PayCount+@Score;";
         private const string integralSql = "select ClassId,UserId,DataId as ItemId,Title,Score,AddTime from integral_record_Table1 where UserId=@UserId and ClassId=@ClassId "
@@ -31,17 +33,17 @@ namespace Integral.WebApi.Controllers
         private const string billSql = "select Integral_Count as AllCount,PayCount,UnionId,Integral_Userid as UserId,UserName from integral_area where AreaCode=@AreaCode and AreaId=@AreaId and UnionId=@UnionId order by AllCount desc,PayCount desc limit @Limit offset @Offset";
         private const string updateAreaSql = "UPDATE integral_area set AreaId = @AreaId,UserName = @UserName where Integral_Userid=@UserId";
         private const string areaSql= "SELECT sum(Integral_Count) as AllCount,sum(PayCount) as PaysCount,AreaId from integral_area GROUP BY AreaId";
-  //      private const string daySql = "SELECT Score,AreaId,Day  from integral_day where  Day>=@Date and AreaId=@AreaId and ClassId is null order by Day,AreaId,ClassId";
-        private const string classSql = "SELECT Score,AreaId,Day,ClassId  from integral_day where  Day>=@Date and AreaId=@AreaId and ClassId=@ClassId order by AreaId,Day,ClassId";
-        private const string monthSql = "SELECT Score,AreaId,Month,ClassId  from integral_month where  Month>=@Date and AreaId=@AreaId and ClassId=@ClassId order by AreaId,Month,ClassId";
+        private const string classSql = "SELECT Score,AreaId,Day,ClassId  from integral_day where  Day>=@Date and Day<=@Date2 and AreaId=@AreaId and ClassId=@ClassId order by AreaId,Day,ClassId";
+        private const string monthSql = "SELECT Score,AreaId,Month,ClassId  from integral_month where  Month>=@Date and Month<=@Date2 and AreaId=@AreaId and ClassId=@ClassId order by AreaId,Month,ClassId";
         private const string interSql = "SELECT ELT(INTERVAL (Integral_Count, 0,10*@Count, 20*@Count, 30*@Count, 40*@Count,50*@Count,60*@Count,70*@Count,80*@Count,90*@Count,100*@Count),CONCAT('0-',10*@Count),CONCAT(10*@Count,'-',20*@Count),CONCAT(20*@Count,'-',30*@Count),CONCAT(30*@Count,'-',40*@Count),CONCAT(40*@Count,'-',50*@Count),CONCAT(50*@Count,'-',60*@Count),CONCAT(60*@Count,'-',70*@Count),CONCAT(70*@Count,'-',80*@Count),CONCAT(80*@Count,'-',90*@Count),CONCAT(90*@Count,'-',100*@Count),CONCAT(100*@Count,'+')) AS intName,count(Integral_Count) AS intCount  FROM integral_area GROUP BY intName order by INTERVAL (Integral_Count, 0,10*@Count, 20*@Count, 30*@Count, 40*@Count,50*@Count,60*@Count,70*@Count,80*@Count,90*@Count,100*@Count)";
         private const string topSql = "SELECT sum(Integral_Count),sum(Integral_Count)-sum(PayCount),max(Integral_Count),max(Integral_Count-PayCount) from integral_area";
-        public IntegralController(ILoggerFactory logFactory, IDapperFactory dapperFactory, IIntegralConfigStore configStore)
+        public IntegralController(ILoggerFactory logFactory, IIntegralConfigStore configStore, IDapperPlusWrite dapperPlusWrite, IDapperPlusRead dapperPlusRead)
         {
             this.configStore = configStore;
             _logger = logFactory.CreateLogger<IntegralController>();
-            if (_dapperClient == null)
-                _dapperClient = dapperFactory.CreateClient("Default");
+
+            if (_dapperPlusWrite == null) _dapperPlusWrite = dapperPlusWrite;
+            if (_dapperPlusRead == null) _dapperPlusRead = dapperPlusRead;
         }
 
         /// <summary>
@@ -61,7 +63,7 @@ namespace Integral.WebApi.Controllers
             string intSql = integralSql.Replace("Table1", $"{areaCode}{DateTime.Now.Year - 2020}_" + DateTime.Now.Month);
             intSql = intSql.Replace("Table2", DateTime.Now.Month == 1 ? $"{areaCode}{DateTime.Now.Year - 2021}_12" : $"{areaCode}{DateTime.Now.Year - 2020}_" + (DateTime.Now.Month - 1));
 			if(string.IsNullOrEmpty(classId))intSql = intSql.Replace("ClassId=", "ClassId!=");
-            var result = await _dapperClient.QueryAsync<IntegralRecord>(intSql, new { UserId = userId, ClassId = classId,Limit = pageSize, Offset = pageIndex * pageSize });
+            var result = await _dapperPlusRead.QueryAsync<IntegralRecord>(intSql, new { UserId = userId, ClassId = classId,Limit = pageSize, Offset = pageIndex * pageSize });
 
             return Ok(result);
         }
@@ -74,13 +76,13 @@ namespace Integral.WebApi.Controllers
         [HttpGet("My")]
         public async Task<IActionResult> GetMyIntegral(string areaCode, string userId)
         {
-            var result = await _dapperClient.QueryFirstAsync<ValueTuple<int, int, string>>(mySql, new { AreaCode = areaCode, UserId = userId });
+            var result = await _dapperPlusRead.QueryFirstAsync<ValueTuple<int, int, string>>(mySql, new { AreaCode = areaCode, UserId = userId });
             return Ok(result);
         }
 
 
-        [HttpGet("day/{date}")]
-        public async Task<IActionResult> GetMydayclass([FromServices] DaprClient daprClient, string date, string classId, string areaId = "")
+        [HttpGet("day/{date}/{date2}")]
+        public async Task<IActionResult> GetMydayclass(string date, string date2, string classId, string areaId = "")
         {
             string daySql;
             if (date.Length == 10)
@@ -89,9 +91,8 @@ namespace Integral.WebApi.Controllers
             }
             else{
                 daySql = string.IsNullOrEmpty(classId) ? monthSql.Replace("=@ClassId", " is not null") : monthSql;
-            }
-            
-            var result = await _dapperClient.QueryAsync<ValueTuple<int, string, string, string>>(areaId.Length == 0 ? daySql.Replace("AreaId=", "AreaId!=") : daySql, new { Date = date, ClassId= classId, AreaId = areaId });
+            }            
+            var result = await _dapperPlusRead.QueryAsync<ValueTuple<int, string, string, string>>(areaId.Length == 0 ? daySql.Replace("AreaId=", "AreaId!=") : daySql, new { Date = date, Date2 = date2, ClassId = classId, AreaId = areaId });
     
             return Ok(result);
         }
@@ -99,21 +100,21 @@ namespace Integral.WebApi.Controllers
         [HttpGet("toparea")]
         public async Task<IActionResult> GetToparea()
         {
-            var result = await _dapperClient.QueryAsync<ValueTuple<int, int, string>>(areaSql,"");
+            var result = await _dapperPlusRead.QueryAsync<ValueTuple<int, int, string>>(areaSql,"");
             return Ok(result);
         }
 
         [HttpGet("topmax")]
         public async Task<IActionResult> GetTopmax()
         {
-            var result = await _dapperClient.QueryAsync<ValueTuple<int, int, int, int>>(topSql, "");
+            var result = await _dapperPlusRead.QueryAsync<ValueTuple<int, int, int, int>>(topSql, "");
             return Ok(result);
         }
 
         [HttpGet("inter")]
         public async Task<IActionResult> GetInter(uint count=1, string areaId = "")
         {
-            var result = await _dapperClient.QueryAsync<ValueTuple<string, int>>(areaId.Length >10 ? interSql.Replace("GROUP", "where AreaId=@AreaId GROUP") : interSql, new { Count = count, AreaId = areaId });
+            var result = await _dapperPlusRead.QueryAsync<ValueTuple<string, int>>(areaId.Length >10 ? interSql.Replace("GROUP", "where AreaId=@AreaId GROUP") : interSql, new { Count = count, AreaId = areaId });
             return Ok(result);
         }
 
@@ -142,10 +143,10 @@ namespace Integral.WebApi.Controllers
                 countsql = countsql.Replace("UnionId=@UnionId", "1=1");
             }
             if (pageIndex < 1) pageIndex = 1;
-            var result = await _dapperClient.QueryAsync<TopUser>(mysql,
+            var result = await _dapperPlusRead.QueryAsync<TopUser>(mysql,
                 new { AreaCode = areaCode, AreaId = areaId, UnionId = unionId, Limit = pageSize, Offset = (pageIndex-1) * pageSize });
             
-            var count = await _dapperClient.QueryFirstAsync<int>(countsql, new { areaCode, areaId, unionId });
+            var count = await _dapperPlusRead.QueryFirstAsync<int>(countsql, new { areaCode, areaId, unionId });
             return CommonPagedResponseDto<IList<TopUser>>.BuildOK(result, pageIndex, pageSize, count);
         }
 
@@ -169,14 +170,14 @@ namespace Integral.WebApi.Controllers
                 UserId = webApiInput.UserId
             };
             if (record.Score == 0|| record.AreaCode=="") return Ok(false);
-            int result = await _dapperClient.ExecuteAsync(updateSql, record);
+            int result = await _dapperPlusWrite.ExecuteAsync(updateSql, record);
             if (result == 0)
             {
                 _logger.LogInformation(webApiInput.UserId + " 积分兑换不足");
                 return Ok(false);
             }
-            await _dapperClient.ExecuteAsync(spendSql, record);
-            _logger.LogInformation(webApiInput.UserId + " 积分兑换花费分数：" + record.Score);
+            await _dapperPlusWrite.ExecuteAsync(spendSql, webApiInput);
+            _logger.LogInformation(webApiInput.UserId + " 积分兑换花费分数：" + webApiInput.Score);
             return Ok(true);
         }
 
@@ -191,29 +192,10 @@ namespace Integral.WebApi.Controllers
         {
             var areaInput = EncryptionHelper.BaseAseDecrypt<AreaInput>(ClaptrapCodes.Key, dto.Value);
             if (areaInput == null) return Ok(false);
-            int result = await _dapperClient.ExecuteAsync(updateAreaSql, areaInput);
+            int result = await _dapperPlusWrite.ExecuteAsync(updateAreaSql, areaInput);
             return Ok(result>0);
         }
-        /*           /// <summary>
-                  /// 获取区域积分配置信息
-                  /// </summary>
-                  /// <param name="daprClient"></param>
-                  /// <param name="areaCode">区域编码</param>
-                  /// <returns></returns>
-                  [HttpGet("init/{areaCode}")]
-             public async Task<IActionResult> InitScore([FromServices] DaprClient daprClient, string areaCode = "51")
-              {
-                  var configList = await _dapperClient.QueryAsync<IntegralConfig>(ClaptrapCodes.InitSql, new { AreaCode = areaCode + "%" });
-                  foreach (var ilist in configList)
-                  {
-                      configStore.Save(ilist);
-                      var state = await daprClient.GetStateEntryAsync<IntegralConfig>(ClaptrapCodes.StoreName, ilist.ClassId);
-                      state.Value = ilist;
-                      await state.SaveAsync();
-                  }
-                  _logger.LogInformation($"初始化市州{areaCode}积分配置{configList.Count}条成功！");
-                  return Ok(configList);
-              }*/
+
 
         [HttpPost("Saveconfig")]
         public async Task<IActionResult> SaveConfig([FromBody] AppParameterDto dto)
@@ -223,7 +205,7 @@ namespace Integral.WebApi.Controllers
             int result = 0;
             try
             {
-                result = await _dapperClient.ExecuteAsync(InsertConfigSql, config);
+                result = await _dapperPlusWrite.ExecuteAsync(InsertConfigSql, config);
                 if (result > 0)
                 {
                     configStore.Save(config);
